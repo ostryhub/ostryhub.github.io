dd64c0eb0a15eaeef612c52eed86831d7886e479
Rafal <ostrowski.rafal0@gmail.com>
2023-12-07 17:25:39 +0100

    Firebase storage plugged in

---
+++ alpha/ltbl/init-firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCyUB25rntQ3HxIrHbsF01S4UcS_zh2Jy8",
+    authDomain: "cadnodes.firebaseapp.com",
+    projectId: "cadnodes",
+    storageBucket: "cadnodes.appspot.com",
+    messagingSenderId: "239718959770",
+    appId: "1:239718959770:web:e7aa98dd69107bedb5ed23",
+    measurementId: "G-FC3LWRB5ZG"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- alpha/ltbl/inject.js
+++ alpha/ltbl/inject.js
@@ -1,8 +1,37 @@
 console.log("src/inject.js: Injecting deps into embedded frame window frames[0] is ",frames[0])
-console.log("just injected webpackNumbers into iframe, frames[0].window", frames[0].window)
-console.log("Injecting embedded frame window")
 
-frames[0].window.webpackNumbers = window.webpackNumbers;
-frames[0].window.webpackNumbers.init(frames[0].window);
+// (() => {
+    console.log("just injected webpackNumbers into iframe, frames[0].window", frames[0].window)
+    
+    frames[0].window.webpackNumbers = window.webpackNumbers;
+    frames[0].window.webpackNumbers.init(frames[0].window);
+// })();
 
+// (() => {
+//     console.log("Injecting firebaseProvider: ",firebaseProvider)
+
+    console.log("Injecting firebaseConfig: ",firebaseConfig)
+    console.log("Injecting firebase: ",firebase)
+
+    frames[0].window.firebaseConfig = firebaseConfig;
+    frames[0].window.firebase = firebase;
+
+    // const p = firebaseProvider;
+    // frames[0].window.firebaseConfig = p.firebaseConfig;
+    //
+    // const { initializeApp } = p.initializeApp;
+    // // const { getAnalytics } = p.getAnalytics;
+    // // const { getAuthentication } = p.getAuthentication;
+    // // const { getStorage } = p.getStorage;
+    // // const { getFirestore } = p.getFirestore;
+    //
+    // // Initialize Firebase
+    // const app = initializeApp(p.firebaseConfig);
+    // frames[0].window.firebase = app;
+    // // frames[0].window.analytics = getAnalytics(app);
+    // // frames[0].window.authentication = getAuthentication(app);
+    // // frames[0].window.storage = getStorage(app);
+    // // frames[0].window.firestore = getFirestore(app);
+    
+// })();
 
